a53499c5509243a1eb85321b45a6331b2eb7f9ad
Mohammad Rizki Prambudi <79072340+MohammadRizkiPrambudi@users.noreply.github.com>
2025-07-16 07:45:03 +0700

    Update Role Pembeli

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
@@ -17,16 +15,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|confirmed|min:8',
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+            'name'     => $request->name,
+            'email'    => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        $user->assignRole('pembeli');
 
         return redirect()->route('login')->with('success', 'Register berhasil, silakan login.');
     }
